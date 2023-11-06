63d73506dd6abfab92e0e365b24e7726ef6e40c5
Karina Timofeeva <kar.timofeeva93@gmail.com>
2023-11-06 13:01:12 +0100

    Updated tests/capitalize.test.js withassert test logic

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,11 +1,18 @@
 import capitalize from "../src/capitalize.js";
+import assert from "assert"; // Import the assert library
+// if (capitalize("hello") !== "Hello") {
+//   throw new Error("Функция работает неверно!");
+// }
 
-if (capitalize("hello") !== "Hello") {
-  throw new Error("Функция работает неверно!");
-}
+// if (capitalize("") !== "") {
+//   throw new Error("Функция работает неверно!");
+// }
 
-if (capitalize("") !== "") {
-  throw new Error("Функция работает неверно!");
-}
+// console.log("Все тесты пройдены!");
+assert.equal(capitalize(""), "");
+// Первый параметр actual – то, что пришло
+// Второй параметр expected – то, что ожидает тест
+// Правильный порядок аргументов имеет большое значение при анализе ошибки
+assert.equal(capitalize("hello"), "Hello");
 
-console.log("Все тесты пройдены!");
+console.log("All tests passed!");
